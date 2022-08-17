6deacfa2fb6c024a46cab75cd7991beecc0c631d
JUDITH MAYRA ESPINOZA TORREZ <judyespinoza.jmet@gmail.com>
2022-08-17 14:18:06 -0400

    Update save.php
    campos añadidos

--- niveles_configuracion/save.php
+++ niveles_configuracion/save.php
@@ -10,18 +10,20 @@ $dbh = new Conexion();
 $nombre=$_POST["nombre"];
 $abreviatura=$_POST["abreviatura"];
 $campoForaneo=$_POST["campo_foraneo"];
+$campoForaneo2=$_POST["campo_foraneo2"];
 $nivel=$_POST["nivel"];
 $codEstado="1";
 
 //echo $nombre." ".$abreviatura." ".$table;
 // Prepare
-$stmt = $dbh->prepare("INSERT INTO $table (nombre, abreviatura, cod_estado, nivel, $keyForeignTable) VALUES (:nombre, :abreviatura, :cod_estado, :nivel, :campo_foraneo)");
+$stmt = $dbh->prepare("INSERT INTO $table (nombre, abreviatura, cod_estado, nivel,$keyForeignTable,$keyForeignTable2) VALUES (:nombre, :abreviatura, :cod_estado, :nivel, :campo_foraneo, :campo_foraneo2)");
 // Bind
 $stmt->bindParam(':nombre', $nombre);
 $stmt->bindParam(':abreviatura', $abreviatura);
 $stmt->bindParam(':cod_estado', $codEstado);
 $stmt->bindParam(':nivel', $nivel);
 $stmt->bindParam(':campo_foraneo', $campoForaneo);
+$stmt->bindParam(':campo_foraneo2', $campoForaneo2);
 
 $flagSuccess=$stmt->execute();
 showAlertSuccessError($flagSuccess,$urlList);
