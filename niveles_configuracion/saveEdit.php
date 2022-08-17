03e71d2fe8cd5b3850b1e2577273bd7c68425149
JUDITH MAYRA ESPINOZA TORREZ <judyespinoza.jmet@gmail.com>
2022-08-17 14:18:15 -0400

    Update saveEdit.php
    campos añadidos

--- niveles_configuracion/saveEdit.php
+++ niveles_configuracion/saveEdit.php
@@ -10,17 +10,20 @@ $dbh = new Conexion();
 $codigo=$_POST["codigo"];
 $nombre=$_POST["nombre"];
 $abreviatura=$_POST["abreviatura"];
+$nivel=$_POST["nivel"];
 $campoForaneo=$_POST["campo_foraneo"];
+$campoForaneo2=$_POST["campo_foraneo2"];
 $codEstado="1";
 
 // Prepare
-$stmt = $dbh->prepare("UPDATE $table set nombre=:nombre, abreviatura=:abreviatura, $keyForeignTable=:campo_foraneo where codigo=:codigo");
+$stmt = $dbh->prepare("UPDATE $table set nombre=:nombre, abreviatura=:abreviatura, nivel=:nivel,  $keyForeignTable=:campo_foraneo, $keyForeignTable2=:campo_foraneo2 where codigo=:codigo");
 // Bind
 $stmt->bindParam(':codigo', $codigo);
 $stmt->bindParam(':nombre', $nombre);
 $stmt->bindParam(':abreviatura', $abreviatura);
 $stmt->bindParam(':campo_foraneo', $campoForaneo);
-
+$stmt->bindParam(':campo_foraneo2', $campoForaneo2);
+$stmt->bindParam(':nivel', $nivel);
 $flagSuccess=$stmt->execute();
 showAlertSuccessError($flagSuccess,$urlList);
 
