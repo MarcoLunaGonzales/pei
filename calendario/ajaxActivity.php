4eed42da0320a150aaf47f10baa5d4fb8904cfca
roalxDcar <roalmollericona@gmail.com>
2022-09-21 17:49:50 -0400

    .

--- calendario/ajaxActivity.php
+++ calendario/ajaxActivity.php
@@ -12,8 +12,8 @@ $dbh = new Conexion();
 $cod_personal = $_SESSION['globalUser'];
 
 // Obtiene fecha de Calendario
-$fecha_inicio = $_GET['start'];
-$fecha_final  = $_GET['end'];
+$fecha_inicio = date("Y-m-d H:i:s", strtotime($_GET['start']));
+$fecha_final  = date("Y-m-d H:i:s", strtotime($_GET['end']));
 
 // Consulta de Actividades Fechas de Inicio
 $sqlActStart="	SELECT 1 as type, a.codigo as code, a.nombre as title, a.observaciones as description, DATE_FORMAT(aces.fecha,'%Y-%m-%d') as start, ('bg-primary') className
@@ -23,7 +23,8 @@ $sqlActStart="	SELECT 1 as type, a.codigo as code, a.nombre as title, a.observac
     LEFT JOIN actividades_cambios_estado aces ON aces.cod_actividad = a.codigo
     WHERE a.cod_padre is null
     AND aces.cod_estadoactividad = 1
-    AND (a.cod_responsable = '$cod_personal' OR aco.cod_personal = '$cod_personal')";
+    AND (a.cod_responsable = '$cod_personal' OR aco.cod_personal = '$cod_personal')
+    AND aces.fecha BETWEEN '$fecha_inicio' AND '$fecha_final'";
 $stmtActivityStart = $dbh->prepare($sqlActStart);
 $stmtActivityStart->execute();
 $rowsActivityStart = $stmtActivityStart->fetchAll(PDO::FETCH_ASSOC);
@@ -34,7 +35,8 @@ $sqlActEnd="	SELECT 1 as type, a.codigo as code, a.nombre as title, a.observacio
     LEFT JOIN actividades_prioridades ap ON a.cod_prioridad = ap.codigo
     LEFT JOIN actividades_colaboradores aco ON aco.cod_actividad = a.codigo
     WHERE a.cod_padre is null
-    AND (a.cod_responsable = '$cod_personal' OR aco.cod_personal = '$cod_personal')";
+    AND (a.cod_responsable = '$cod_personal' OR aco.cod_personal = '$cod_personal')
+    AND a.fecha_limite BETWEEN '$fecha_inicio' AND '$fecha_final'";
 $stmtActivityEnd = $dbh->prepare($sqlActEnd);
 $stmtActivityEnd->execute();
 $rowsActivityEnd = $stmtActivityEnd->fetchAll(PDO::FETCH_ASSOC);
@@ -46,7 +48,8 @@ $sqlHitos ="	SELECT 2 as type, ahi.nombre as title, a.observaciones as descripti
     LEFT JOIN actividades_colaboradores aco ON aco.cod_actividad = a.codigo
     LEFT JOIN actividades_hitos ahi ON ahi.cod_actividad = a.codigo
     WHERE a.cod_padre is null
-    AND (a.cod_responsable = '$cod_personal' OR aco.cod_personal = '$cod_personal')";
+    AND (a.cod_responsable = '$cod_personal' OR aco.cod_personal = '$cod_personal')
+    AND ahi.fecha_hito BETWEEN '$fecha_inicio' AND '$fecha_final'";
 $stmtHitos = $dbh->prepare($sqlHitos);
 $stmtHitos->execute();
 $rowsHitos = $stmtHitos->fetchAll(PDO::FETCH_ASSOC);
@@ -54,7 +57,8 @@ $rowsHitos = $stmtHitos->fetchAll(PDO::FETCH_ASSOC);
 // Eventos Personales
 $sqlEventos ="	SELECT 3 as type, e.detalle as title, e.observacion as description, DATE_FORMAT(e.fecha_inicio,'%Y-%m-%d %H:%i:%s') as start, DATE_FORMAT(e.fecha_fin,'%Y-%m-%d %H:%i:%s') as end, ('bg-warning') className
     from eventos e
-    WHERE e.cod_personal = '$cod_personal'";
+    WHERE e.cod_personal = '$cod_personal'
+    AND e.fecha_inicio BETWEEN '$fecha_inicio' AND '$fecha_final'";
 $stmtEventos = $dbh->prepare($sqlEventos);
 $stmtEventos->execute();
 $rowsEventos = $stmtEventos->fetchAll(PDO::FETCH_ASSOC);
